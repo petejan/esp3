bf776149df34cfb92eebe5b06a70a6ff6f01422a
unknown <dunford@NIWA-34413.niwa.local>
2016-02-04 12:37:48 +1300

    Add optional code to compensate for vessel draught.
    Former-commit-id: 9543de26c01335eaf758f13fce61b0a26febb38d [formerly 9cdbe0ca85142bb3fed067b2da4f509dfb727b51]
    Former-commit-id: 09d87dbb37cd9b5fc5ea6cbd89493e45e5f36a19
    Former-commit-id: b5e9dc544b6b63fd6fc41b187935b8e022aa2e99
    Former-commit-id: 15a3025f1f6aa774490df38aded8d91517a8d54f [formerly 9d37cfc9c1304c2acc942d3d45d5d187ef44ef26]
    Former-commit-id: d5bd7b0acb5cce49c233dc22846b714d06559903
    Former-commit-id: 07001f7c671ef171d02af53f7010bfc55bb17e13

--- fileIO/import_lines/create_line_from_rbr_mat.m
+++ fileIO/import_lines/create_line_from_rbr_mat.m
@@ -9,6 +9,8 @@ function obj=create_line_from_rbr_mat(filename)
         [Y,M,D,H,MN,S] = datevec(RBR.sampletimes{i},'dd/mm/yyyy HH:MM:SS.FFF PM');
         timestamp(i) = datenum(Y,M,D,H,MN,S);
     end
-    depth = RBR.data(:,4);
+    depth = RBR.data(:,4);     % uncomment lines below to compensate for vessel draught
+    % draught = 6;             % (m) draught of vessel (Tangaroa is 6m)
+    % depth = depth - draught; % RBR depth is DBS and echogram depth is DBT
     obj=line_cl('Tag','Imported from RBR','Range',depth,'Time',timestamp-12/24,'File_origin',filename,'UTC_diff',-12);
 end
