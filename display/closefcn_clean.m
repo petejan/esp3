bf54df6071f51cb3b030ce0235f2cabd4d5b753c
Yoann Ladroit <yoann.ladroit@niwa.co.nz>
2017-03-07 08:51:07 +1300

    -Fixing commit... Not sure what is happenning there

--- display/closefcn_clean.m
+++ display/closefcn_clean.m
@@ -1,16 +1,53 @@
-function closefcn_clean(main_fig,~)
-% User-defined close request function
-% to display a question dialog box
-check_saved_bot_reg(main_fig);
-selection=close_dialog_fig(main_fig);
-% selection = questdlg('Close The Program?',...
-%     '',...
-%     'Yes','No','Yes');
-switch selection,
+function closefcn_clean(main_figure,~)
+% closefcn_clean(main_figure,~)
+%
+% DESCRIPTION
+%
+% User-defined close request function (figure property 'CloseRequestFcn')
+% for ESP3.
+%
+% USE
+%
+% Only called when the ESP3 main_figure is being closed, to ensure ESP3
+% exits in a clean state
+%
+% PROCESSING SUMMARY
+%
+% - Check if there are unsaved bottom and regions
+% - Open Close dialog box
+% - Clean up the app before closing
+%
+% INPUT VARIABLES
+%
+% - main_figure (required): ESP3 main figure
+%
+% RESEARCH NOTES
+%
+% NA
+%
+% NEW FEATURES
+%
+% 2017-03-02: Comments and header Alex
+%
+%%%
+% Yoann Ladroit, NIWA
+%%%
+
+% Check if there are unsaved bottom and regions
+check_saved_bot_reg(main_figure);
+
+% Open Close dialog box
+selection=close_dialog_fig(main_figure);
+
+% Handle answer
+switch selection
     case 'Yes'
-       cleanup_echo(main_fig);
+        % Clean up the app before closing
+       cleanup_echo(main_figure);
     case 'No'
         return;
 end
+
+
 
 end
--- display/main_figure/initialize_interactions.m
+++ display/main_figure/initialize_interactions.m
@@ -1,5 +1,6 @@
 function initialize_interactions(main_figure,new)
 % initialize_interactions(main_figure)
+<<<<<<< HEAD
 %
 % DESCRIPTION
 %
@@ -30,6 +31,41 @@ function initialize_interactions(main_figure,new)
 % 2017-03-02: first version.
 %
 %%%
+=======
+% 
+% DESCRIPTION 
+% 
+% Initialize user interactions with ESP3 main figure
+% 
+% USE 
+% 
+% [This section contains a more detailed description of what the function does and how to use it, for the interested user to have an overall understanding of its function] 
+% This is a text file containing the basic comment template to add at the
+% start of any new ESP3 function to serve as function help. 
+% [REPLACE THESE LINES WITH ACTUAL CONTENT OR DELETE IF UNUSED]
+% 
+% PROCESSING SUMMARY 
+% 
+% [This section contains bullet point list of major processing steps, for the very interested user to have a clear understanding of the function works before reading its details]
+% - Function does this first
+% - Then it does this
+% [REPLACE THESE LINES WITH ACTUAL CONTENT OR DELETE IF UNUSED]
+% 
+% INPUT VARIABLES 
+% 
+% - main_figure (required): ESP3 main figure
+% 
+% RESEARCH NOTES 
+% 
+% [This section describes what features are temporary, needed future developments and paper references.]
+% [REPLACE THESE LINES WITH ACTUAL CONTENT OR DELETE IF UNUSED]
+% 
+% NEW FEATURES 
+% 
+% 2017-03-02: first version. 
+% 
+%%% 
+>>>>>>> b34623d06e293d3bf0bd1fdb788fee6bbab3525c
 % Yoann Ladroit, NIWA.
 %%%
 
