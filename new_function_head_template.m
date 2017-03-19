d17b180c9ec436590bb9d32e564e58a105e73831
Alexandre Schimel <alexandre.schimel@niwa.co.nz>
2017-03-20 10:59:38 +1300

    new header template

---
+++ new_function_head_template.m
@@ -0,0 +1,80 @@
+%% my_Matlab_function_name.m
+%
+% _This section contains a very short description of the function, for the
+% user to know this is part of ESP3 and what it is. Example below to
+% replace. Delete these lines._ 
+%
+% Template of ESP3 function header.
+%
+%% Help
+%
+% *USE*
+%
+% _This section contains a more detailed description of what the function
+% does and how to use it, for the interested user to have an overall
+% understanding of its function. Example below to replace. Delete these
+% lines._  
+%
+% This is a text file containing the basic comment template to add at the
+% start of any new ESP3 function to serve as function help. 
+%
+% *INPUT VARIABLES*
+%
+% _This section contains bullet points of input variables with types and
+% description. Example below to replace. Delete these lines._  
+%
+% 'output_variable_1' (required). Valid Options:
+%
+% * char: description
+% * cell: description
+%
+% 'output_variable_2' (optional). Valid Options:
+%
+% * 1xN numeric array: description
+%
+% 'output_variable_3' (parameter). Valid Options:
+%
+% * struct: description
+%
+% *OUTPUT VARIABLES*
+%
+% _This section contains bullet points of output variables. Example below
+% to replace. Delete these lines._ 
+%
+% * 'output_variable_1': type and description
+% * 'output_variable_2': type and description
+%
+% *RESEARCH NOTES*
+%
+% _This section describes what features are temporary, needed future
+% developments and paper references. Example below to replace. Delete these lines._
+%
+% * research point 1
+% * research point 2
+%
+% *NEW FEATURES*
+%
+% _This section contains dates and descriptions of major updates. Example
+% below to replace. Delete these lines._
+%
+% * YYYY-MM-DD: second version. Describes the update.
+% * YYYY-MM-DD: first version.
+%
+% *EXAMPLE*
+%
+% _This section contains examples of valid function calls. Note that
+% example lines start with 3 white spaces so that the publish function
+% shows them correctly as matlab code. Example below to replace. Delete
+% these lines._ 
+%
+%   example_use_1; % comment on what this does.
+%   example_use_2: % comment on what this line does.
+%
+% *AUTHOR, AFFILIATION & COPYRIGHT*
+%
+% _This last section contains at least author name and affiliation. Delete
+% these lines._ 
+%
+% Yoann Ladroit, NIWA. Type |help EchoAnalysis.m| for copyright information.
+
+%% Function
