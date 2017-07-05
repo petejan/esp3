f2d203e85638daa3977642af2822f62259eaff23
Yoann Ladroit <yoann.ladroit@niwa.co.nz>
2017-07-05 16:48:06 +1200

    -improvement in integration (again....)

--- classes/@transceiver_cl/integrate_region_v2.m
+++ classes/@transceiver_cl/integrate_region_v2.m
@@ -334,11 +334,11 @@ x_mat_idx=floor(bsxfun(@minus,x_mat,x_mat(:,1))/cell_w)+1;
 
 switch region.Reference
     case {'Bottom' 'Line'}
-        y_mat_idx=ceil(y_mat/cell_h);
-        y_mat_idx=y_mat_idx-min(y_mat_idx(~isinf(y_mat_idx)&Mask_reg_min_bot))+1;
+        y_mat_idx=ceil(y_mat/cell_h);       
     otherwise
-        y_mat_idx=floor(bsxfun(@minus,y_mat,y_mat(1,:))/cell_h)+1;
+        y_mat_idx=floor(y_mat/cell_h);        
 end
+y_mat_idx=y_mat_idx-min(y_mat_idx(~isinf(y_mat_idx)&Mask_reg_min_bot))+1;
 
 Sv_reg_lin(~Mask_reg_min_bot)=nan;
 
