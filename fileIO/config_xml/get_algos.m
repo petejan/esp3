84c35c357fdb07baf017a0f14c50cdd27938b313
Alex Schimel <alexandre.schimel@niwa.co.nz>
2017-07-10 14:15:55 +1200

    - some comments on a couple functions - started a XML template for algos

--- fileIO/config_xml/get_algos.m
+++ fileIO/config_xml/get_algos.m
@@ -1,25 +1,78 @@
-function algo_cell=get_algos(algo_node)
-nb_algos=length(algo_node.Children);
-algo_cell=cell(1,nb_algos);
-for i=1:nb_algos
+%% get_algos.m
+%
+% TODO: write short description of function
+%
+%% Help
+%
+% *USE*
+%
+% TODO: write longer description of function
+%
+% *INPUT VARIABLES*
+%
+% * |input_variable_1|: TODO: write description and info on variable
+%
+% *OUTPUT VARIABLES*
+%
+% * |output_variable_1|: TODO: write description and info on variable
+%
+% *RESEARCH NOTES*
+%
+% TODO: write research notes
+%
+% *NEW FEATURES*
+%
+% * 2017-07-06: start commenting and header (Alex Schimel).
+% * YYYY-MM-DD: first version (Author). TODO: complete date and comment
+%
+% *EXAMPLE*
+%
+% TODO: write examples
+%
+% *AUTHOR, AFFILIATION & COPYRIGHT*
+%
+% Yoann Ladroit, NIWA. Type |help EchoAnalysis.m| for copyright information.
+
+%% Function
+function algo_cell = get_algos(algo_node)
+
+% number of algorithms
+nb_algos = length(algo_node.Children);
+
+% initialize output
+algo_cell = cell(1,nb_algos);
+
+% get each algo details
+for i = 1:nb_algos
+
+    % ignore comments
     if strcmp(algo_node.Children(i).Name,'#comment')
         continue;
     end
-    algo_cell{i}.Name=algo_node.Children(i).Name;
-    for j=1:length(algo_node.Children(i).Attributes)
-        algo_cell{i}.Varargin.(algo_node.Children(i).Attributes(j).Name)=algo_node.Children(i).Attributes(j).Value;
+    
+    % record algo name
+    algo_cell{i}.Name = algo_node.Children(i).Name;
+    
+    % record each attribute
+    for j = 1:length(algo_node.Children(i).Attributes)
+        algo_cell{i}.Varargin.(algo_node.Children(i).Attributes(j).Name) = algo_node.Children(i).Attributes(j).Value;
     end
     
+    % special record for frequencies if this field exists
     if isfield(algo_cell{i}.Varargin,'Frequencies')
         if ischar(algo_cell{i}.Varargin.Frequencies)
-            algo_cell{i}.Varargin.Frequencies=str2double(strsplit(algo_cell{i}.Varargin.Frequencies,';'));
+            algo_cell{i}.Varargin.Frequencies = str2double(strsplit(algo_cell{i}.Varargin.Frequencies,';'));
             if isnan(algo_cell{i}.Varargin.Frequencies)
-                algo_cell{i}.Varargin.Frequencies=[];
+                algo_cell{i}.Varargin.Frequencies = [];
             end
         end
     else
-        algo_cell{i}.Varargin.Frequencies=[];
+        algo_cell{i}.Varargin.Frequencies = [];
     end
+    
 end
-algo_cell(cellfun(@isempty,algo_cell))=[];
+
+% remove empty algorithms (comments)
+algo_cell(cellfun(@isempty,algo_cell)) = [];
+
 end
